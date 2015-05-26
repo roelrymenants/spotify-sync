310d9e3fe3bc32dfbe360d4a2ed398a459bee9b1
Roel Rymenants <roelrymenants@users.noreply.github.com>
2015-05-26 18:57:43 +0000

    Reworked backend to github.com/zmb3/spotify

--- spotify-sync.go
+++ spotify-sync.go
@@ -7,16 +7,19 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/GeertJohan/go.rice"
 	"github.com/gorilla/context"
 	"github.com/gorilla/sessions"
-	"github.com/roelrymenants/spotify"
+	"github.com/zmb3/spotify"
 )
 
 const (
-	DefaultWwwRoot  = "/home/nitrous/www/"
+	DefaultWwwRoot  = "www"
 	clientId        = "b57e5e2127014c8d9e45da5e507936bb"
 	clientSecret    = "8b980072b2cf4f52ba05a75b7391ba46"
 	DefaultAuthFile = "/home/nitrous/spotify-sync.auth-"
+	RedirectUrl     = "http://go-102859.nitrousapp.com:8080/callback"
+	state           = "123" //Default state
 )
 
 type Page struct {
@@ -26,10 +29,21 @@ type Page struct {
 
 var store = sessions.NewCookieStore([]byte("spotify-sync-secret"))
 
-var connections map[string]*spotify.SpotifyConnection
+var connections map[string]*spotify.Client
 
 type connectionHandler struct {
-	h http.Handler
+	h             http.Handler
+	Authenticator *spotify.Authenticator
+}
+
+func newConnectionHandler() *connectionHandler {
+	auth := spotify.NewAuthenticator(RedirectUrl, spotify.ScopeUserLibraryRead)
+	auth.SetAuthInfo(clientId, clientSecret)
+
+	return &connectionHandler{
+		h:             http.DefaultServeMux,
+		Authenticator: &auth,
+	}
 }
 
 func (c connectionHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -40,9 +54,19 @@ func (c connectionHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	context.Set(r, "login", login)
 
 	connection, connectionOk := connections[login]
-	if !connectionOk && loginOk {
-		connection = spotify.NewConnection(clientId, clientSecret)
-		connection.Load(authFile + login)
+	log.Print(r.URL.Path)
+	if !connectionOk && loginOk && r.URL.Path == "/callback" {
+		token, err := c.Authenticator.Token(state, r)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tConn := c.Authenticator.NewClient(token)
+		connection = &tConn
+		connections[login] = connection
+
+		http.Redirect(w, r, "/", 301)
 	}
 
 	context.Set(r, "connection", connection)
@@ -57,32 +81,20 @@ func main() {
 	authFile = *flag.String("authFile", DefaultAuthFile, "File to load/save auth data from/to")
 	flag.Parse()
 
+	wwwBox := rice.MustFindBox(wwwRoot)
+
+	connections = make(map[string]*spotify.Client)
+
 	http.HandleFunc("/api/trackList", func(w http.ResponseWriter, r *http.Request) {
-		connection := context.Get(r, "connection").(*spotify.SpotifyConnection)
-		trackList := connection.GetSpotifyTrackList()
+		connection := context.Get(r, "connection").(*spotify.Client)
+		trackList, err := connection.CurrentUsersTracks()
+
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		encoder := json.NewEncoder(w)
 		encoder.Encode(trackList)
-	})
-
-	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
-		connection := context.Get(r, "connection").(*spotify.SpotifyConnection)
-		authCode := r.FormValue("code")
-		login := context.Get(r, "login").(string)
-
-		err := connection.DoAuth(authCode, "http://go-102859.nitrousapp.com:8080/callback")
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = connection.Save(authFile + login)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		http.Redirect(w, r, "/", 301)
 	})
 
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
@@ -105,21 +117,46 @@ func main() {
 		http.Redirect(w, r, "/", 301)
 	})
 
+	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		//Hack dummy handlefunc
+	})
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		connection := context.Get(r, "connection").(*spotify.SpotifyConnection)
+		connection := context.Get(r, "connection").(*spotify.Client)
 
 		var page Page
 
 		page.Login = context.Get(r, "login").(string)
 
-		if connection != nil && connection.Authentication != nil {
-			log.Printf("%v %v", connection, connection.Authentication)
+		if connection != nil && connection != nil {
+			log.Printf("%v %v", connection)
 			page.Authenticated = true
 		}
 
-		t, _ := template.ParseFiles(wwwRoot + "index.html")
-		t.Execute(w, page)
+		template, err := loadTemplate("index", wwwBox)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		template.Execute(w, page)
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", context.ClearHandler(connectionHandler{http.DefaultServeMux})))
+	log.Fatal(http.ListenAndServe(":8080", context.ClearHandler(newConnectionHandler())))
+}
+
+func loadTemplate(name string, wwwBox *rice.Box) (t *template.Template, err error) {
+	templateString, err := wwwBox.String("index.tmpl")
+
+	if err != nil {
+		return nil, err
+	}
+
+	t, err = template.New("index").Parse(templateString)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return
 }
